6e14a2a089b616f39356be4706283bccafafd848
Zwilla <info@zwilla.de>
2017-02-21 13:06:50 +0100

    Update api.php
    formatting the code

--- api.php
+++ api.php
@@ -1,35 +1,57 @@
 <?php
-#API IS DISABLED BY DEFAULT AS ITS STILL IN DEV. ENABLE AT YOUR OWN RISK.
-define("IN_WALLET", false); #TO ENABLE SET TO TRUE
+#API HAS TO BE DISABLED BY DEFAULT AS ITS STILL IN DEV. ENABLE AT YOUR OWN RISK.
+define("IN_WALLET", true); #TO ENABLE SET TO TRUE
 ###################################################
 
 include('common.php');
 $con = mysqli_connect("$db_host","$db_user","$db_pass","$db_name");
 $key = $_GET['key'];
-if (mysqli_connect_errno()) {
+
+if (mysqli_connect_errno())
+{
     echo "Failed to connect to MySQL. Make sure to edit the common.php file: " . mysqli_connect_error();
 }
+
 $result = mysqli_query($con,"SELECT * FROM users where secret = '$key' and authused=1");
 
-while($row = mysqli_fetch_array($result)) {
-    $id = $row['id'];
-    $username = $row['username'];
-    $pin = $row['supportpin'];
+while($row = mysqli_fetch_array($result))
+{
+    $id          = $row['id'];
+    $username    = $row['username'];
+    $pin         = $row['supportpin'];
     $twofactoren = $row['authused'];
-    $isadmin = $row['admin'];
-    $client = new Client($rpc_host, $rpc_port, $rpc_user, $rpc_pass);
-    $apibal = $client->getBalance($username) - $fee;
-    $addr = $client->getAddress($username);
+    $isadmin     = $row['admin'];
+    $client      = new Client($rpc_host, $rpc_port, $rpc_user, $rpc_pass);
+    $apibal      = $client->getBalance($username) - $fee;
+    $addr        = $client->getAddress($username);
 }
-if ($isadmin == 1) {
+
+if ($isadmin == 1) 
+{
     $ifadmin = "true";
-} else {
+} 
+else 
+    {
     $ifadmin = "false";
-}
-if ($twofactoren == 1) {
+    }
+    
+if ($twofactoren == 1)
+{
     $iftwofactor = "true";
-} else {
+} 
+else
+    {
     $iftwofactor = "false";
-}
-echo json_encode(array("id" => "$id", "username" => "$username", "balance" => "$apibal", "addresses" => "$addr", "Support Pin" => "$pin", "Two Factor Enabled"=> "$iftwofactor", "admin" => "$ifadmin"));
+    }
+    
+echo json_encode(
+    [
+        "id"                 => "$id", 
+        "username"           => "$username", 
+        "balance"            => "$apibal", 
+        "addresses"          => "$addr", 
+        "Support Pin"        => "$pin", 
+        "Two Factor Enabled" => "$iftwofactor", 
+        "admin"              => "$ifadmin"
+    ]);
 ?>
