86c33ee1fbf6265a738695297974e5ac4410d5e5
Zwilla <info@zwilla.de>
2017-02-21 13:50:58 +0100

    Update Client.php

--- classes/Client.php
+++ classes/Client.php
@@ -1,49 +1,130 @@
 <?php if (!defined("IN_WALLET")) { die("Auth Error!"); } ?>
 <?php
-//To enable developer mode (no need for an RPC server, replace this file with the snipet at https://gist.github.com/d3e148deb5969c0e4b60 
+//To enable developer mode (no need for an RPC server, set $devmode to 1
 
-class Client {
-	private $uri;
-	private $jsonrpc;
+/**
+ * Class Client
+ */
+class Client
+{
+    private $uri;
+    private $jsonrpc;
+    private $devmode = 0;
 
-	function __construct($host, $port, $user, $pass)
-	{
-		$this->uri = "http://" . $user . ":" . $pass . "@" . $host . ":" . $port . "/";
-		$this->jsonrpc = new jsonRPCClient($this->uri);
-	}
 
-	function getBalance($user_session)
-	{
-		return $this->jsonrpc->getbalance("zelles(" . $user_session . ")", 6);
-		//return 21;
-	}
+    /**
+     * Client constructor.
+     * @param $host
+     * @param $port
+     * @param $user
+     * @param $pass
+     */
+    function __construct($host, $port, $user, $pass)
+    {
+        $this->uri     = "https://" . $user . ":" . $pass . "@" . $host . ":" . $port . "/";
+        $this->jsonrpc = new jsonRPCClient($this->uri);
+    }
 
-       function getAddress($user_session)
+
+    /**
+     * @param $user_session
+     * @return int
+     */
+    function getBalance($user_session)
+    {
+        if ($devmode == 0)
         {
-                return $this->jsonrpc->getaccountaddress("zelles(" . $user_session . ")");
-	}
+            return $this->jsonrpc->getbalance("zelles(" . $user_session . ")", 6);
+        }
+        else
+            {
+                return 21;
+            }
+    }
 
-	function getAddressList($user_session)
-	{
-		return $this->jsonrpc->getaddressesbyaccount("zelles(" . $user_session . ")");
-		//return array("1test", "1test");
-	}
 
-	function getTransactionList($user_session)
-	{
-		return $this->jsonrpc->listtransactions("zelles(" . $user_session . ")", 10);
-	}
+    /**
+     * @param $user_session
+     * @return array
+     */
+    function getAddress($user_session)
+    {
+        if ($devmode == 0) {
+            return $this->jsonrpc->getaccountaddress("zelles(" . $user_session . ")");
+        }
+        else
+        {
+            return ["1test", "1test"];
+        }
+    }
 
-	function getNewAddress($user_session)
-	{
-		return $this->jsonrpc->getnewaddress("zelles(" . $user_session . ")");
-		//return "1test";
-	}
 
-	function withdraw($user_session, $address, $amount)
-	{
-		return $this->jsonrpc->sendfrom("zelles(" . $user_session . ")", $address, (float)$amount, 6);
-		//return "ok wow";
-	}
+    /**
+     * @param $user_session
+     * @return array
+     */
+    function getAddressList($user_session)
+    {
+        if ($devmode == 0)
+        {
+            return $this->jsonrpc->getaddressesbyaccount("zelles(" . $user_session . ")");
+        } 
+        else
+            {
+                return ["1test", "1test"];
+            }
+
+    }
+
+
+    /**
+     * @param $user_session
+     * @return mixed
+     */
+    function getTransactionList($user_session)
+    {
+        if ($devmode == 0)
+        {
+            return $this->jsonrpc->listtransactions("zelles(" . $user_session . ")", 10);
+        }
+    }
+
+
+    /**
+     * @param $user_session
+     * @return string
+     */
+    function getNewAddress($user_session)
+    {
+        if ($devmode == 0)
+        {
+            return $this->jsonrpc->getnewaddress("zelles(" . $user_session . ")");
+        }
+        else
+            {
+                return "1test";
+            }
+    }
+
+
+    /**
+     * @param $user_session
+     * @param $address
+     * @param $amount
+     * @return string
+     */
+    function withdraw($user_session, $address, $amount)
+    {
+        if ($devmode == 0)
+        {
+            return $this->jsonrpc->sendfrom("zelles(" . $user_session . ")", $address, (float)$amount, 6);
+        } 
+        else
+            {
+                return "ok wow";
+            }
+    }
 }
+
+
 ?>
